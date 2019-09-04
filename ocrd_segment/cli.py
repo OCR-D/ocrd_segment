9a9a40a6067ad224029f8e265465fb869ab36a45
Robert Schubert <robert.schubert@mathint.com>
2019-09-04 12:54:35 +0200

    evaluate: first (trivial) version

--- ocrd_segment/cli.py
+++ ocrd_segment/cli.py
@@ -2,8 +2,14 @@ import click
 
 from ocrd.decorators import ocrd_cli_options, ocrd_cli_wrap_processor
 from ocrd_segment.repair import RepairSegmentation
+from ocrd_segment.evaluate import EvaluateSegmentation
 
 @click.command()
 @ocrd_cli_options
 def ocrd_segment_repair(*args, **kwargs):
     return ocrd_cli_wrap_processor(RepairSegmentation, *args, **kwargs)
+
+@click.command()
+@ocrd_cli_options
+def ocrd_segment_evaluate(*args, **kwargs):
+    return ocrd_cli_wrap_processor(EvaluateSegmentation, *args, **kwargs)
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 Installs:
 
     - ocrd-segment-repair
+    - ocrd-segment-evaluate
 """
 import codecs
 
@@ -10,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ocrd_segment',
-    version='0.0.1',
+    version='0.0.2',
     description='Page segmentation and segmentation evaluation',
     long_description=codecs.open('README.md', encoding='utf-8').read(),
     author='Konstantin Baierer, Kay-Michael Würzner, Robert Sachunsky',
@@ -25,6 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             'ocrd-segment-repair=ocrd_segment.cli:ocrd_segment_repair',
+            'ocrd-segment-evaluate=ocrd_segment.cli:ocrd_segment_evaluate',
         ]
     },
 )
