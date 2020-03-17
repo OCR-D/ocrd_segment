963c78b2a55ff5f2c7918dbd2c34a67dfed972e1
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2020-03-17 13:43:34 +0100

    extract-pages: also export COCO format...
    - put binarized and debug images into extra file groups
      (if given): raw[, binarized[, debug]]
    - catch/ignore missing binarized images
    - omit white hull border around masks in debug images
    - use-subtype colors in debug images (if present)
    - export COCO for all pages:
      - map PAGE region types to (super)categories
        (and subtypes to categories), `source=PAGE`
      - map numeric part of pageId to image_id
      - convert coords polygon

--- ocrd_segment/extract_pages.py
+++ ocrd_segment/extract_pages.py
@@ -2,16 +2,23 @@ from __future__ import absolute_import
 
 import json
 from PIL import Image, ImageDraw
+from shapely.geometry import Polygon
 
 from ocrd_utils import (
     getLogger, concat_padded,
     coordinates_of_segment,
+    xywh_from_polygon,
     MIME_TO_EXT
 )
 from ocrd_models.ocrd_page import (
     LabelsType, LabelType,
     MetadataItemType
 )
+from ocrd_models.ocrd_page_generateds import (
+    TextTypeSimpleType,
+    GraphicsTypeSimpleType,
+    ChartTypeSimpleType
+)    
 from ocrd_modelfactory import page_from_file
 from ocrd import Processor
 
@@ -51,22 +58,76 @@ class ExtractPages(Processor):
         Get all regions with their types (region element class), sub-types (@type)
         and coordinates relative to the page (which depending on the workflow could
         already be cropped, deskewed, dewarped, binarized etc). Extract the image of
-        the page, both in binarized and non-binarized form. In addition, create a new
-        image which color-codes all regions. Create a JSON file with region types and
-        coordinates.
+        the (cropped, deskewed, dewarped) page, both in binarized form (if available)
+        and non-binarized form. In addition, create a new image with masks for all
+        regions, color-coded by type. Create two JSON files with region types and
+        coordinates: one (page-wise) in our custom format and one (global) in MS-COCO.
         
-        Write all files in the directory of the output file group, named like so:
-        * ID + '.png': raw image
-        * ID + '.bin.png': binarized image
-        * ID + '.dbg.png': debug image
-        * ID + '.json': region coordinates
+        The output file group may be given as a comma-separated list to separate
+        these 3 page-level images. Write files as follows:
+        * in the directory of the first (or only) output file group:
+          - ID + '.png': raw image of the (preprocessed) page
+          - ID + '.json': region coordinates/classes (custom format)
+        * in the directory of the second (or first) output file group:
+          - ID + '.bin.png': binarized image of the (preprocessed) page, if available
+        * in the directory of the third (or first) output file group:
+          - ID + '.dbg.png': debug image
+        
+        In addition, write a file for all pages in the parent directory, named like so:
+        * output_file_grp + '.coco.json': region coordinates/classes (MS-COCO)
         
         (This is intended for training and evaluation of region segmentation models.)
         """
+        file_groups = self.output_file_grp.split(',')
+        if len(file_groups) > 3:
+            raise Exception("at most 3 output file grps allowed (raw, [binarized, [debug]] image)")
+        if len(file_groups) > 2:
+            dbg_image_grp = file_groups[2]
+        else:
+            dbg_image_grp = file_groups[0]
+            LOG.info("No output file group for debug images specified, falling back to output filegrp '%s'", dbg_image_grp)
+        if len(file_groups) > 1:
+            bin_image_grp = file_groups[1]
+        else:
+            bin_image_grp = file_groups[0]
+            LOG.info("No output file group for binarized images specified, falling back to output filegrp '%s'", bin_image_grp)
+        self.output_file_grp = file_groups[0]
+        
+        # COCO: init data structures
+        images = list()
+        annotations = list()
+        categories = list()
+        i = 0
+        for cat, color in CLASSES.items():
+            categories.append(
+                {'id': i, 'name': cat, 'supercategory': '',
+                 'source': 'PAGE', 'color': color})
+            i += 1
+        typedict = {"text": TextTypeSimpleType,
+                    "graphic": GraphicsTypeSimpleType,
+                    "chart": ChartTypeSimpleType}
+        i = len(categories)
+        SUPERCLASSES = dict()
+        for supercat, class_ in typedict.items():
+            j = i
+            for name, cat in vars(class_).items():
+                if name[0] != '_':
+                    color = list(CLASSES[supercat])
+                    for c in range(3):
+                        if not color[c]:
+                            color[c] = (i-j+1) * 5
+                    SUPERCLASSES[(cat, supercat)] = tuple(color)
+                    categories.append(
+                        {'id': i, 'name': cat, 'supercategory': supercat,
+                         'source': 'PAGE', 'color': color})
+                    i += 1
+
+        i = 0
         # pylint: disable=attribute-defined-outside-init
         for n, input_file in enumerate(self.input_files):
             file_id = input_file.ID.replace(self.input_file_grp, self.output_file_grp)
             page_id = input_file.pageId or input_file.ID
+            num_page_id = int(page_id.strip(page_id.strip("0123456789")))
             LOG.info("INPUT FILE %i / %s", n, page_id)
             pcgts = page_from_file(self.workspace.download_file(input_file))
             page = pcgts.get_Page()
@@ -97,14 +158,20 @@ class ExtractPages(Processor):
                                                        self.output_file_grp,
                                                        page_id=page_id,
                                                        mimetype=self.parameter['mimetype'])
-            page_image_bin, _, _ = self.workspace.image_from_page(
-                page, page_id,
-                feature_selector='binarized',
-                transparency=self.parameter['transparency'])
-            self.workspace.save_image_file(page_image_bin,
-                                           file_id + '.bin',
-                                           self.output_file_grp,
-                                           page_id=page_id)
+            try:
+                page_image_bin, _, _ = self.workspace.image_from_page(
+                    page, page_id,
+                    feature_selector='binarized',
+                    transparency=self.parameter['transparency'])
+                self.workspace.save_image_file(page_image_bin,
+                                               file_id + '.bin',
+                                               bin_image_grp,
+                                               page_id=page_id)
+            except Exception as err:
+                if err.args[0].startswith('Found no AlternativeImage'):
+                    LOG.warning('Page "%s" has no binarized images, skipping .bin', page_id)
+                else:
+                    raise
             page_image_dbg = Image.new(mode='RGB', size=page_image.size, color=0)
             regions = { 'text': page.get_TextRegion(),
                         'table': page.get_TableRegion(),
@@ -122,12 +189,18 @@ class ExtractPages(Processor):
             description = { 'angle': page.get_orientation() }
             for rtype, rlist in regions.items():
                 for region in rlist:
+                    subrtype = region.get_type() if rtype in ['text', 'chart', 'graphic'] else None
                     polygon = coordinates_of_segment(
-                        region, page_image, page_coords).tolist()
+                        region, page_image, page_coords)
+                    polygon2 = polygon.reshape(1,-1).tolist()
+                    polygon = polygon.tolist()
+                    xywh = xywh_from_polygon(polygon)
+                    area = Polygon(polygon).area
                     description.setdefault('regions', []).append(
                         { 'type': rtype,
-                          'subtype': region.get_type() if rtype in ['text', 'chart', 'graphic'] else None,
+                          'subtype': subrtype,
                           'coords': polygon,
+                          'area': area,
                           'features': page_coords['features'],
                           'DPI': dpi,
                           'region.ID': region.id,
@@ -136,14 +209,42 @@ class ExtractPages(Processor):
                           'file_grp': self.input_file_grp,
                           'METS.UID': self.workspace.mets.unique_identifier
                         })
-                    ImageDraw.Draw(page_image_dbg).polygon(list(map(tuple, polygon)),
-                                                               fill=CLASSES[rtype])
-                    ImageDraw.Draw(page_image_dbg).line(list(map(tuple, polygon + [polygon[0]])), 
-                                                            fill=CLASSES['border'], width=3)
+                    # draw region:
+                    ImageDraw.Draw(page_image_dbg).polygon(
+                        list(map(tuple, polygon)),
+                        fill=SUPERCLASSES.get((subrtype, rtype), CLASSES.get(rtype)))
+                    # draw hull:
+                    #ImageDraw.Draw(page_image_dbg).line(
+                    #    list(map(tuple, polygon + [polygon[0]])), 
+                    #    fill=CLASSES['border'], width=3)
+                    # COCO: add annotations
+                    i += 1
+                    annotations.append(
+                        {'id': i, 'image_id': num_page_id,
+                         'category_id': next((cat['id'] for cat in categories if cat['name'] == subrtype),
+                                             next((cat['id'] for cat in categories if cat['name'] == rtype))),
+                         'segmentation': polygon2,
+                         'area': area,
+                         'bbox': [xywh['x'], xywh['y'], xywh['w'], xywh['h']],
+                         'iscrowd': 0})
+            
             self.workspace.save_image_file(page_image_dbg,
                                            file_id + '.dbg',
-                                           self.output_file_grp,
+                                           dbg_image_grp,
                                            page_id=page_id,
                                            mimetype=self.parameter['mimetype'])
             file_path = file_path.replace(MIME_TO_EXT[self.parameter['mimetype']], '.json')
-            json.dump(description, open(file_path, 'w'))
+            with open(file_path, 'w') as out:
+                json.dump(description, out)
+
+            # COCO: add image
+            images.append(
+                {'id': num_page_id, 'file_name': page.imageFilename,
+                 'width': page_image.width, 'height': page_image.height})
+        
+        # COCO: write result
+        with open(self.output_file_grp + '.coco.json', 'w') as coco:
+            json.dump({'categories': categories,
+                       'images': images,
+                       'annotations': annotations},
+                      coco)
