9dbb9d1cdcb38759408cc241d50c5297ee7229a3
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2020-11-25 20:49:38 +0100

    replace-original: only ensure coord validity, not consistency

--- ocrd_segment/replace_original.py
+++ ocrd_segment/replace_original.py
@@ -19,7 +19,7 @@ from ocrd_modelfactory import page_from_file
 from ocrd import Processor
 
 from .config import OCRD_TOOL
-from .repair import ensure_consistent
+from .repair import ensure_valid
 
 TOOL = 'ocrd-segment-replace-original'
 
@@ -95,20 +95,20 @@ class ReplaceOriginal(Processor):
                 for region in page.get_AllRegions():
                     region_polygon = coordinates_of_segment(region, page_image, page_coords)
                     region.get_Coords().set_points(points_from_polygon(region_polygon))
-                    ensure_consistent(region)
+                    ensure_valid(region)
                     if isinstance(region, TextRegionType):
                         for line in region.get_TextLine():
                             line_polygon = coordinates_of_segment(line, page_image, page_coords)
                             line.get_Coords().set_points(points_from_polygon(line_polygon))
-                            ensure_consistent(line)
+                            ensure_valid(line)
                             for word in line.get_Word():
                                 word_polygon = coordinates_of_segment(word, page_image, page_coords)
                                 word.get_Coords().set_points(points_from_polygon(word_polygon))
-                                ensure_consistent(word)
+                                ensure_valid(word)
                                 for glyph in word.get_Glyph():
                                     glyph_polygon = coordinates_of_segment(glyph, page_image, page_coords)
                                     glyph.get_Coords().set_points(points_from_polygon(glyph_polygon))
-                                    ensure_consistent(glyph)
+                                    ensure_valid(glyph)
 
             # update METS (add the PAGE file):
             out = self.workspace.add_file(
